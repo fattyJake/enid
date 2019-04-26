1bcfdac1115e9a441b9d7425834f7cbb755525e2
fattyJake <jacobwang123@gmail.com>
2019-04-25 21:38:20 -0400

    full sentence sampling

--- claim2vec_op.pyx
+++ claim2vec_op.pyx
@@ -1,17 +1,20 @@
 import numpy as np
 cimport numpy as np
-import random
+from itertools import chain
 
+ctypedef np.int_t DTYPE_t
 DTYPE = np.int
 
 cdef class SkipGram:
-    cdef int data_index, group_index, epoch, batch_size
+    cdef int data_index, group_index, epoch, batch_size, input_cache
+    cdef DTYPE_t[:] context_cache
 
     def __init__(self, int batch_size):
         self.data_index = 0
         self.group_index = 0
         self.epoch = 0
         self.batch_size = batch_size
+        self.input_cache = -1
 
     def get_data_index(self):
         return self.data_index
@@ -23,8 +26,8 @@ cdef class SkipGram:
         return self.epoch
 
     def __call__(self, list data):
-        batch = np.ndarray(shape=(self.batch_size), dtype=DTYPE)
-        labels = np.ndarray(shape=(self.batch_size, 1), dtype=DTYPE)
+        cdef np.ndarray[DTYPE_t, ndim=1] batch  = np.ndarray(shape=(self.batch_size), dtype=DTYPE)
+        cdef np.ndarray[DTYPE_t, ndim=2] labels = np.ndarray(shape=(self.batch_size, 1), dtype=DTYPE)
         if self.group_index >= len(data)-1:
             self.epoch += 1
             self.data_index = 0
@@ -35,24 +38,37 @@ cdef class SkipGram:
             self.group_index = self.group_index + 1
         
         # locate group
-        data_ = data[self.group_index]
-        i = 0
+        cdef np.ndarray[DTYPE_t, ndim=1] data_ = np.array(data[self.group_index])
+        cdef int i = 0
+        cdef DTYPE_t input_
+        cdef list slides
+        cdef np.ndarray[DTYPE_t, ndim=1] context_words
+
         while True:
-            input_ = data_[self.data_index]
-            if len(data_)-1 > self.batch_size-i: context_words = random.sample([data_[w] for w in range(len(data_)) if w != self.data_index], self.batch_size-i)
-            else: context_words = [w for w in data_ if w != input_]
+            if self.input_cache < 0:
+                input_ = data_[self.data_index]
+                slides = list(chain(range(self.data_index), range(self.data_index+1, data_.shape[0])))
+                if data_.shape[0]-1 > self.batch_size-i:
+                    context_words = data_[slides[:self.batch_size-i]]
+                    self.input_cache = input_
+                    self.context_cache = data_[slides[self.batch_size-i:]]
+                else:
+                    context_words = data_[slides]
+            else:
+                input_ = self.input_cache
+                context_words = np.asarray(self.context_cache)
+                self.input_cache = -1
             for context_word in context_words:
                 batch[i] = input_
                 labels[i, 0] = context_word
                 i += 1
                 if i >= self.batch_size:
-                    self.data_index += 1
                     return batch, labels
             self.data_index += 1
-            if self.data_index == len(data_):
+            if self.data_index == data_.shape[0]:
                 if self.group_index < len(data)-1: self.group_index += 1
                 else:
                     self.group_index = 0
                     self.epoch += 1
-                data_ = data[self.group_index]
+                data_ = np.array(data[self.group_index])
                 self.data_index = 0
